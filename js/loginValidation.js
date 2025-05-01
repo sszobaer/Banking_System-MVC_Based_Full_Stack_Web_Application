add2c3e43b43472b2fba44298ef9215613a7aaab
sszobaer <22-49415-3@student.aiub.edu>
2025-05-01 19:52:58 +0600

    Dashboard design added

--- js/loginValidation.js
+++ js/loginValidation.js
@@ -1,16 +1,29 @@
 let emailValidation = () => {
   const email = document.getElementById("email").value.trim();
-  const error = document.getElementById("emailError");
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const errorDiv = document.getElementById("emailError");
 
   if (email === "") {
-      error.textContent = "Please enter your email.";
-      return false;
-  } else if (!regex.test(email)) {
-      error.textContent = "Invalid email format.";
-      return false;
+    errorDiv.textContent = "Please enter your email.";
+    return false;
   }
-  error.textContent = "";
+
+  if (!email.includes("@") || !email.includes(".")) {
+    errorDiv.textContent = "Email must contain '@' and '.'";
+    return false;
+  }
+
+  const atPosition = email.indexOf("@");
+  const dotPosition = email.lastIndexOf(".");
+
+  if (
+    atPosition < 1 || dotPosition < atPosition + 2 ||
+    dotPosition + 1 >= email.length
+  ) {
+    errorDiv.textContent = "Invalid email format.";
+    return false;
+  }
+
+  errorDiv.textContent = "";
   return true;
 };
 
@@ -19,17 +32,20 @@ let passwordValidation = () => {
   const error = document.getElementById("passwordError");
 
   if (password.length < 6) {
-      error.textContent = "Password must be at least 6 characters.";
-      return false;
+    error.textContent = "Password must be at least 6 characters.";
+    return false;
   }
   error.textContent = "";
   return true;
 };
 
-let loginValidation = () => {
-  const isValid = emailValidation() && passwordValidation();
-  if (isValid) {
-      window.location.href = "dashboard.html"; 
+const loginBtn = document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click", (event) => {
+  event.preventDefault(); 
+  if(emailValidation() && passwordValidation()) {
+    alert("Login successful!");
+    window.location.href = "userDashboard.html";
   }
-  return false; 
-};
+});
+
