9dc8f2846f8c14a9cc832d992da8a8ab3ae8d311
sszobaer <22-49415-3@student.aiub.edu>
2025-05-03 01:07:47 +0600

    Modify the login validation and made more accurate

--- js/loginValidation.js
+++ js/loginValidation.js
@@ -16,7 +16,8 @@ let emailValidation = () => {
   const dotPosition = email.lastIndexOf(".");
 
   if (
-    atPosition < 1 || dotPosition < atPosition + 2 ||
+    atPosition < 1 ||
+    dotPosition < atPosition + 2 ||
     dotPosition + 1 >= email.length
   ) {
     errorDiv.textContent = "Invalid email format.";
@@ -30,7 +31,10 @@ let emailValidation = () => {
 let passwordValidation = () => {
   const password = document.getElementById("password").value.trim();
   const error = document.getElementById("passwordError");
-
+  if(!password){
+    error.textContent = "Password is requied";
+    return false;
+  }
   if (password.length < 6) {
     error.textContent = "Password must be at least 6 characters.";
     return false;
@@ -40,12 +44,23 @@ let passwordValidation = () => {
 };
 
 const loginBtn = document.getElementById("loginBtn");
+const headerLoginBtn = document.getElementById("login-btn");
 
 loginBtn.addEventListener("click", (event) => {
-  event.preventDefault(); 
-  if(emailValidation() && passwordValidation()) {
-    alert("Login successful!");
-    window.location.href = "userDashboard.html";
+  event.preventDefault();
+  if (emailValidation() && passwordValidation()) {
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+    if (email === "user@fake.com" && password === "user123") {
+      alert("Login successful!");
+      window.location.href = "userDashboard.html";
+
+    } else if (email === "admin@fake.com" && password === "admin123") {
+      alert("Login successful!");
+      window.location.href = "adminDashboard.html";
+      headerLoginBtn.textContent = "LogOut";
+    } else {
+      alert("email or password is invalid");
+    }
   }
 });
-
