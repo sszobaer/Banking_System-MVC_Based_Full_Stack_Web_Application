0b89e1b83b55f8c77cadaec57453e961cb969dea
sszobaer <22-49415-3@student.aiub.edu>
2025-05-03 01:13:57 +0600

    Add logic for editing user/admin profiles

---
+++ js/editProfile.js
@@ -0,0 +1,160 @@
+let nameValidation = () => {
+    const firstName = document.getElementById("firstName").value.trim();
+    const lastName = document.getElementById("lastName").value.trim();
+    const firstNameError = document.getElementById("firstNameError");
+    const lastNameError = document.getElementById("lastNameError");
+  
+    let isValid = true;
+  
+    if (firstName === "") {
+      firstNameError.textContent = "Please enter your first name.";
+      isValid = false;
+    } else {
+      firstNameError.textContent = "";
+    }
+  
+    if (lastName === "") {
+      lastNameError.textContent = "Please enter your last name.";
+      isValid = false;
+    } else {
+      lastNameError.textContent = "";
+    }
+  
+    return isValid;
+  };
+  
+  let emailValidation = () => {
+    const email = document.getElementById("email").value.trim();
+    const errorDiv = document.getElementById("emailError");
+  
+    if (email === "") {
+      errorDiv.textContent = "Please enter your email.";
+      return false;
+    }
+  
+    if (!email.includes("@") || !email.includes(".")) {
+      errorDiv.textContent = "Email must contain '@' and '.'";
+      return false;
+    }
+  
+    const atPosition = email.indexOf("@");
+    const dotPosition = email.lastIndexOf(".");
+  
+    if (
+      atPosition < 1 ||
+      dotPosition < atPosition + 2 ||
+      dotPosition + 1 >= email.length
+    ) {
+      errorDiv.textContent = "Invalid email format.";
+      return false;
+    }
+  
+    errorDiv.textContent = "";
+    return true;
+  };
+  
+  let phoneValidation = () => {
+    const phoneNo = document.getElementById("phone").value.trim();
+    const errorDiv = document.getElementById("phoneError");
+  
+    if (phoneNo === "") {
+      errorDiv.textContent = "Please enter your phone number.";
+      return false;
+    } else if (phoneNo.length !== 11) {
+      errorDiv.textContent = "Phone number must be 11 digits.";
+      return false;
+    }
+  
+    for (let i = 0; i < phoneNo.length; i++) {
+      if (phoneNo[i] < "0" || phoneNo[i] > "9") {
+        errorDiv.textContent = "Phone number must contain only digits.";
+        return false;
+      }
+    }
+    errorDiv.textContent = "";
+    return true;
+  };
+  
+  let dobValidation = () => {
+    const dob = document.getElementById("dob").value.trim();
+    const errorDiv = document.getElementById("dobError");
+  
+    if (dob === "") {
+      errorDiv.textContent = "Please enter your date of birth.";
+      return false;
+    }
+  
+    errorDiv.textContent = "";
+    return true;
+  };
+  
+  let genderValidation = () => {
+    const genders = document.getElementsByName("gender");
+    const errorDiv = document.getElementById("genderError");
+    let isSelected = false;
+  
+    for (let gender of genders) {
+      if (gender.checked) {
+        isSelected = true;
+        break;
+      }
+    }
+  
+    if (!isSelected) {
+      errorDiv.textContent = "Please select your gender.";
+      return false;
+    }
+  
+    errorDiv.textContent = "";
+    return true;
+  };
+
+  let presentAddressValidation = () => {
+    const presentAddress = document.getElementById("presentAdress").value.trim();
+    const errorDiv = document.getElementById("presentAdressError");
+  
+    if (presentAddress === "") {
+      errorDiv.textContent = "Present address is required.";
+      errorDiv.style.color = "red";
+      return false;
+    } else {
+      errorDiv.textContent = "";
+      return true;
+    }
+  };
+  let permanentAdressVAlidation = () => {
+    const permanentAddress = document
+      .getElementById("permanentAdress")
+      .value.trim();
+    const errorDiv = document.getElementById("permanentAdressError");
+  
+    if (permanentAddress === "") {
+      errorDiv.textContent = "Permanent address is required.";
+      errorDiv.style.color = "red";
+      return false;
+    } else {
+      errorDiv.textContent = "";
+      return true;
+    }
+  };
+
+  const editProfileValidation = () => {
+    return (
+      nameValidation() &&
+      emailValidation() &&
+      phoneValidation() &&
+      dobValidation() &&
+      genderValidation() &&
+      presentAddressValidation() &&
+      permanentAdressVAlidation() 
+    );
+  };
+  
+  const editButton = document.getElementById("updateProfile");
+  editButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    if (editProfileValidation()) {
+      alert("Your information is updated!");
+    }
+  });
+  
