8f7cfc7977098a5c1e1958ee8a27f330d35b7fa0
sszobaer <22-49415-3@student.aiub.edu>
2025-05-03 01:16:45 +0600

    Add universal logic

--- js/index.js
+++ js/index.js
@@ -1,2 +1,17 @@
+const toggleIcons = document.querySelectorAll(".toggle-password");
+
+toggleIcons.forEach(icon => {
+  icon.addEventListener("click", function () {
+    const input = this.previousElementSibling;
+    if (input.type === "password") {
+      input.type = "text";
+      this.textContent = "🙈";
+    } else {
+      input.type = "password";
+      this.textContent = "👁️";
+    }
+  });
+});
+
 
 
