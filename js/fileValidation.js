8e00e116d3a227a553a82116e38a7cac451daf86
sszobaer <22-49415-3@student.aiub.edu>
2025-05-03 01:17:23 +0600

    Add logic for uploading the profile picture

---
+++ js/fileValidation.js
@@ -0,0 +1,29 @@
+let profilePhotoValidation = () => {
+  const profilePhoto = document.getElementById("profile-photo");
+  const profilePhotoError = document.getElementById("profilePhotoError");
+  const photoFile = profilePhoto.files[0];
+  const isValidImg = ["image/jpg", "image/jpeg", "image/png"];
+
+  if (!photoFile) {
+    profilePhotoError.textContent = "Profile photo is required";
+    profilePhotoError.style.color = "red";
+    return false;
+  } else if (!isValidImg.includes(photoFile.type)) {
+    profilePhotoError.textContent = "Only jpg, jpeg, or png format are allowed";
+    profilePhotoError.style.color = "red";
+    return false;
+  } else {
+    profilePhotoError.textContent = "";
+    return true;
+  }
+};
+
+document
+  .getElementById("profile-change-btn")
+  .addEventListener("click", (event) => {
+    event.preventDefault();
+    if (profilePhotoValidation()) {
+      alert("Your profile has been changed");
+      window.location.href = "userProfile.html";
+    }
+  });
