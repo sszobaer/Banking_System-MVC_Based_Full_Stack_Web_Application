684aa01af1bccae3ca1532e6edf1796f08cf0454
SSZASeyam <118447914+sszobaer@users.noreply.github.com>
2025-05-27 02:47:33 +0600

    No code written right now.

---
+++ view/card.php
@@ -0,0 +1,49 @@
+<!-- ZOBAER AHMED -->
+<?php
+session_start();
+if (isset($_SESSION['email'])) {
+?>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Card</title>
+        <link rel="stylesheet" href="../assets/css/style.css">
+        <link rel="stylesheet" href="../assets/css/_variable.css">
+        <link rel="stylesheet" href="../assets/css/_global.css">
+        <link rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+            integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+            crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link
+            href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"
+            rel="stylesheet">
+    </head>
+
+    <body>
+        <?php
+        include "./header.php";
+        include "./userSidebar.php";
+        ?>
+        <!--------------------Card Management Start-------------------------------->
+        <section class="card-management" id="card-management">
+            <div class="card-management-container">
+                <a href="../view/cardApply.php" class="btn">Apply for Card</a>
+            </div>
+        </section>
+        <!--------------------Card Management End---------------------------------->
+        <script src="../assets/js/cardManagement.js"></script>
+        <script src="../assets/js/index.js"></script>
+    </body>
+
+    </html>
+<?php
+} else {
+    header("Location: login.php");
+    exit();
+}
+?>
