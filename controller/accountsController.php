c1beb43fde74275e92e644f9091769a6fc898497
SSZASeyam <118447914+sszobaer@users.noreply.github.com>
2025-05-27 02:46:18 +0600

    Accounts Controller to control the accounts information.

---
+++ controller/accountsController.php
@@ -0,0 +1,35 @@
+<?php
+require_once '../model/accounts.php';
+require_once '../model/users.php';
+require_once '../model/connection.php';
+//account_id	user_id	account_number	account_type	balance	currency	account_status	created_at	updated_at
+function accountController(){
+    $conn = getConnection();
+
+    $userId = $_GET['approveUser'];
+    $user = [
+        'user_id' => $userId
+    ];
+    $allUsers = fetchIndivisualUserbyID($user);
+
+    $account = [
+        'user_id' => $allUsers['user_id'],
+        'account_number' => rand(1000000000, 9999999999),
+        'account_type' => $allUsers['accountType'],
+        'balance' => $allUsers['depositAmount'],
+        'currency' => 'BDT',
+        'account_status' => 'active',
+        'created_at' => date('Y-m-d H:i:s'),
+        'updated_at' => date('Y-m-d H:i:s')
+    ];
+
+    $status = insertAccounts($account);
+    if($status){
+        approveUserByAdmin($user);
+        header("Location: ../view/userManagement.php");
+        exit();
+    } else{
+        echo "Account creation failed.";
+    } 
+}
+?>
