d2cd9f4adbbab5c2480e34a5d399dcc60ed6cde5
sszobaer <22-49415-3@student.aiub.edu>
2025-05-28 19:22:06 +0600

    Fetch all data from the db using indivisial user_id from user table.

--- Model/pay_bills.php
+++ Model/pay_bills.php
@@ -2,7 +2,6 @@
 require_once '../model/connection.php';
 //bill_id	dest_account_no	biller	currency	payment_amount	memo	receipt	user_id	account_id	account_no	
 
-// Insert a new bill payment
 function insertBillPayment($bill) {
     $conn = getConnection();
     $sql = "INSERT INTO pay_bills (
@@ -39,27 +38,14 @@ function fetchAllBillPayments() {
     }
     return $bills;
 }
-
-function fetchBillPaymentsByUser($user_id) {
+function fetchAllBillPaymentsById($payBill){
     $conn = getConnection();
-    $user_id = mysqli_real_escape_string($conn, $user_id);
-    $sql = "SELECT * FROM pay_bills WHERE user_id = '$user_id' ORDER BY bill_id DESC";
-    $result = mysqli_query($conn, $sql);
-    $bills = [];
-    if ($result && mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            $bills[] = $row;
-        }
-    }
-    return $bills;
-}
 
-function fetchBillPaymentById($bill_id) {
-    $conn = getConnection();
-    $bill_id = mysqli_real_escape_string($conn, $bill_id);
-    $sql = "SELECT * FROM pay_bills WHERE bill_id = '$bill_id'";
+    $sql = "SELECT SUM(payment_amount) AS total_bill_payment FROM pay_bills WHERE user_id = '{$payBill['user_id']}'";
+
     $result = mysqli_query($conn, $sql);
-    if ($result && mysqli_num_rows($result) == 1) {
+    
+    if (mysqli_num_rows($result) == 1) {
         return mysqli_fetch_assoc($result);
     } else {
         return false;
