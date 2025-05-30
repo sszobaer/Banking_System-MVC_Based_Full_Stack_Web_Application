02e68f3d4540c96d859621c8a601598b031e6bbe
SSZASeyam <118447914+sszobaer@users.noreply.github.com>
2025-05-31 00:40:24 +0600

    Destroy the cookie using - time.

--- controller/logoutController.php
+++ controller/logoutController.php
@@ -7,6 +7,10 @@
     session_destroy();
 
     // print_r($_SESSION);
+    setcookie('remember_email', '', time() - 3600, "/");
+    setcookie('remember_password', '', time() - 3600, "/");
+
+
 
     header('Location:../view/login.php');
     exit();
