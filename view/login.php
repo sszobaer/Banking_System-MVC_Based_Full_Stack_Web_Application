d988157e4dcd941dee11728ed3836e3720896522
SSZASeyam <118447914+sszobaer@users.noreply.github.com>
2025-05-31 00:43:09 +0600

    Added remember me checkbox into the login page

--- view/login.php
+++ view/login.php
@@ -1,7 +1,8 @@
 <!-- ZOBAER AHMED -->
 <!DOCTYPE html>
 <html lang="en">
-    <head>
+
+<head>
     <title>Login</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -17,51 +18,56 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"
         rel="stylesheet">
-    </head>
-    <body>
-        <?php
-        include "../view/header.php";
-        ?>
-        <!--------------------Login Start-------------------------------->
-        <section class="reg" id="reg">
-            <div class="regContainer">
-                <h1>Login to your account</h1>
-                <form id="loginForm" method="post" action="../controller/loginController.php">
+</head>
 
-                    <div class="form-group">
-                        <label for="email">Email</label>
-                        <input type="text" id="email" name="email"
-                            placeholder="Enter your email">
-                        <div id="emailError" class="error-message"></div>
-                    </div>
+<body>
+    <?php
+    include "../view/header.php";
+    ?>
+    <!--------------------Login Start-------------------------------->
+    <section class="reg" id="reg">
+        <div class="regContainer">
+            <h1>Login to your account</h1>
+            <form id="loginForm" method="post" action="../controller/loginController.php">
 
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <div class="password-container">
-                            <input type="password" class="password-field"
-                                id="password" name="password"
-                                placeholder="Enter your Password">
-                            <span class="toggle-password eye-icon">👁️</span>
-                        </div>
-                        <div id="passwordError" class="error-message"></div>
-                    </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="text" id="email" name="email" value="<?php echo isset($_COOKIE['remember_email']) ? $_COOKIE['remember_email'] : ''; ?>"
+                        placeholder="Enter your email">
+                    <div id="emailError" class="error-message"></div>
+                </div>
 
-                    <div class="forgot-password">
-                        <a href="./forgotPassword.php">forgot Password?</a>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <div class="password-container">
+                        <input type="password" class="password-field"
+                            id="password" name="password" value="<?php echo isset($_COOKIE['remember_password']) ? $_COOKIE['remember_password'] : ''; ?>"
+                            placeholder="Enter your Password">
+                        <span class="toggle-password eye-icon">👁️</span>
                     </div>
-                    <div class="login-link">
-                        Not registered yet? <a href="./registration.php">Create
-                            Account</a>
-                    </div>
-                    <button class="btn" id="loginBtn">Login</button>
-                </form>
-            </div>
-        </section>
-        <?php
-        include "./footer.php";
-        ?>
-        <!--------------------Login End---------------------------------->
-        <script src="../assets/js/loginValidation.js"></script>
-        <script src="../assets/js/index.js"></script>
-    </body>
+                    <div id="passwordError" class="error-message"></div>
+                </div>
+
+                <div class="forgot-password">
+                    <a href="./forgotPassword.php">forgot Password?</a>
+                </div>
+                <div class="login-link">
+                    Not registered yet? <a href="./registration.php">Create
+                        Account</a>
+                </div>
+                <div class="form-group-terms">
+                    <input type="checkbox" name="remember-me" <?php if (isset($_COOKIE['remember_email'])) echo 'checked'; ?>> Remember Me
+                </div>
+                <button class="btn" id="loginBtn">Login</button>
+            </form>
+        </div>
+    </section>
+    <?php
+    include "./footer.php";
+    ?>
+    <!--------------------Login End---------------------------------->
+    <script src="../assets/js/loginValidation.js"></script>
+    <script src="../assets/js/index.js"></script>
+</body>
+
 </html>
