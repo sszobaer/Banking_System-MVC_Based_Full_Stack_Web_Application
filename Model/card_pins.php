f70502afe2e5c047cb1f67515da8ec5d766a398f
SSZASeyam <118447914+sszobaer@users.noreply.github.com>
2025-05-31 00:46:14 +0600

    Insert data into card pins table.

---
+++ Model/card_pins.php
@@ -0,0 +1,28 @@
+<?php
+require_once "../model/connection.php";
+
+function insertPin($pin){
+    $conn = getConnection();
+
+    $sql = "INSERT INTO card_pins VALUES ('{$pin['pin_id']}', '{$pin['card_id']}','{$pin['pin']}')";
+
+    if (mysqli_query($conn, $sql)) {
+        return true;
+    } else {
+        echo "SQL Error: " . mysqli_error($conn);
+        return false;
+    }
+}
+function fetchAllFromCardPin($pin){
+    $conn = getConnection();
+    $sql = "SELECT * FROM card_pins WHERE card_id = '{$pin['card_id']}'";
+    $result = mysqli_query($conn, $sql);
+    $pins = [];
+    if ($result && mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $pins[] = $row;
+        }
+    }
+    return $pins;
+}
+?>
